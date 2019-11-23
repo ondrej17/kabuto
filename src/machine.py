83e2f25eb93b23228ae0ae7ddfeddced7ccac9db
ondrej17 <bily.ondrej1701@gmail.com>
2019-11-23 21:36:18 +0100

    Demo Neuron Network project - Celsius to Fahrenheit - added saving a loading model

--- src/machine.py
+++ src/machine.py
@@ -1,9 +1,11 @@
-# demo file
+# DEMO FILE
 # converter from celsius to fahrenfeit
 
+import tensorflow_hub as hub
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 # set up an input data
 celsius_q = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
@@ -17,6 +19,14 @@ for ii in range(len(celsius_q)):
 layer_0 = tf.keras.layers.Dense(units=4, input_shape=[1])   # input layer
 layer_1 = tf.keras.layers.Dense(units=4)                    # hidden layer
 layer_2 = tf.keras.layers.Dense(units=1)                    # output layer
+
+# load the Keras .h5 model - uncomment this to load model to the 'reloaded' variable from folder 'models_saved'
+# export_path_keras = './models_saved/1574538278.h5'
+# reloaded = tf.keras.models.load_model(
+#     export_path_keras,
+#     # `custom_objects` tells keras how to load a `hub.KerasLayer`
+#     custom_objects={'KerasLayer': hub.KerasLayer})
+# reloaded.summary()
 
 # create the model
 model = tf.keras.Sequential([layer_0, layer_1, layer_2])
@@ -44,3 +54,10 @@ print("{} degrees Celsius = {} degrees Fahrenheit".format(temperature_c, predict
 print("These are the layer_0 variables: {}\n".format(layer_0.get_weights()))
 print("These are the layer_1 variables: {}\n".format(layer_1.get_weights()))
 print("These are the layer_2 variables: {}\n".format(layer_2.get_weights()))
+
+# save the model as Keras .h5 model
+t = time.time()
+export_path_keras = "./models_saved/{}.h5".format(int(t))
+# uncomment this to save the model
+#model.save(export_path_keras)
+#print("Model saved to file:", export_path_keras)
