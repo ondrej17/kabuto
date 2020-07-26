88e9f809ebb7f166ccfc734b43664495a83ea327
ondrej17 <bily.ondrej1701@gmail.com>
2020-07-26 22:19:07 +0200

    Added bash scripts to run preparation of bcc and fcc phases

---
+++ scripts/run_prepare_bcc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# prepare bcc phase from files in folder: ~/documents/diplomovka/molybden_bcc/
+
+#python3 ../src/kabuto.py prepare bcc ~/documents/diplomovka/molybden_bcc/dump_Mo_bcc_1.out
+python3 ../src/kabuto.py prepare bcc ~/documents/diplomovka/molybden_bcc/dump_Mo_bcc_2.out
+python3 ../src/kabuto.py prepare bcc ~/documents/diplomovka/molybden_bcc/dump_Mo_bcc_3.out
+python3 ../src/kabuto.py prepare bcc ~/documents/diplomovka/molybden_bcc/dump_Mo_bcc_4.out
+python3 ../src/kabuto.py prepare bcc ~/documents/diplomovka/molybden_bcc/dump_Mo_bcc_5.out
+python3 ../src/kabuto.py prepare bcc ~/documents/diplomovka/molybden_bcc/dump_Mo_bcc_6.out
+python3 ../src/kabuto.py prepare bcc ~/documents/diplomovka/molybden_bcc/dump_Mo_bcc_7.out
+python3 ../src/kabuto.py prepare bcc ~/documents/diplomovka/molybden_bcc/dump_Mo_bcc_8.out
+python3 ../src/kabuto.py prepare bcc ~/documents/diplomovka/molybden_bcc/dump_Mo_bcc_9.out
+python3 ../src/kabuto.py prepare bcc ~/documents/diplomovka/molybden_bcc/dump_Mo_bcc_10.out
+python3 ../src/kabuto.py prepare bcc ~/documents/diplomovka/molybden_bcc/dump_Mo_bcc_11.out
+python3 ../src/kabuto.py prepare bcc ~/documents/diplomovka/molybden_bcc/dump_Mo_bcc_12.out
+python3 ../src/kabuto.py prepare bcc ~/documents/diplomovka/molybden_bcc/dump_Mo_bcc_13.out
+python3 ../src/kabuto.py prepare bcc ~/documents/diplomovka/molybden_bcc/dump_Mo_bcc_14.out
+python3 ../src/kabuto.py prepare bcc ~/documents/diplomovka/molybden_bcc/dump_Mo_bcc_15.out
+python3 ../src/kabuto.py prepare bcc ~/documents/diplomovka/molybden_bcc/dump_Mo_bcc_16.out
+python3 ../src/kabuto.py prepare bcc ~/documents/diplomovka/molybden_bcc/dump_Mo_bcc_17.out
+python3 ../src/kabuto.py prepare bcc ~/documents/diplomovka/molybden_bcc/dump_Mo_bcc_18.out
---
+++ scripts/run_prepare_fcc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# prepare fcc phase from files in folder: ~/documents/diplomovka/molybden_fcc/
+
+python3 ../src/kabuto.py prepare fcc ~/documents/diplomovka/molybden_fcc/dump_Mo_fcc_1.out
+python3 ../src/kabuto.py prepare fcc ~/documents/diplomovka/molybden_fcc/dump_Mo_fcc_2.out
+python3 ../src/kabuto.py prepare fcc ~/documents/diplomovka/molybden_fcc/dump_Mo_fcc_3.out
+python3 ../src/kabuto.py prepare fcc ~/documents/diplomovka/molybden_fcc/dump_Mo_fcc_4.out
+python3 ../src/kabuto.py prepare fcc ~/documents/diplomovka/molybden_fcc/dump_Mo_fcc_5.out
+python3 ../src/kabuto.py prepare fcc ~/documents/diplomovka/molybden_fcc/dump_Mo_fcc_6.out
+python3 ../src/kabuto.py prepare fcc ~/documents/diplomovka/molybden_fcc/dump_Mo_fcc_7.out
+python3 ../src/kabuto.py prepare fcc ~/documents/diplomovka/molybden_fcc/dump_Mo_fcc_8.out
+python3 ../src/kabuto.py prepare fcc ~/documents/diplomovka/molybden_fcc/dump_Mo_fcc_9.out
+python3 ../src/kabuto.py prepare fcc ~/documents/diplomovka/molybden_fcc/dump_Mo_fcc_10.out
+python3 ../src/kabuto.py prepare fcc ~/documents/diplomovka/molybden_fcc/dump_Mo_fcc_11.out
+python3 ../src/kabuto.py prepare fcc ~/documents/diplomovka/molybden_fcc/dump_Mo_fcc_12.out
+python3 ../src/kabuto.py prepare fcc ~/documents/diplomovka/molybden_fcc/dump_Mo_fcc_13.out
+python3 ../src/kabuto.py prepare fcc ~/documents/diplomovka/molybden_fcc/dump_Mo_fcc_14.out
+python3 ../src/kabuto.py prepare fcc ~/documents/diplomovka/molybden_fcc/dump_Mo_fcc_15.out
+python3 ../src/kabuto.py prepare fcc ~/documents/diplomovka/molybden_fcc/dump_Mo_fcc_16.out
+python3 ../src/kabuto.py prepare fcc ~/documents/diplomovka/molybden_fcc/dump_Mo_fcc_17.out
+python3 ../src/kabuto.py prepare fcc ~/documents/diplomovka/molybden_fcc/dump_Mo_fcc_18.out
