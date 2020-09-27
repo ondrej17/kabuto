c429aa2ea94f0102e551ce2efce777c506a07a14
ondrej17 <bily.ondrej1701@gmail.com>
2020-09-27 10:33:41 +0200

    C++ extension (descriptors) moved to src/modules/descriptrs folder. Added documentation strings

--- src/modules/descriptors/descriptorsmodule.cpp
+++ src/modules/descriptors/descriptorsmodule.cpp
@@ -3,11 +3,16 @@
 
 #include "descriptorsmodule.h"
 
-/*
- *  Functions
- */
+// C function that are to be used in Python
 
-// function that computes and returns descriptors for one timestep
+/**
+  * Function that computes and returns descriptors for one timestep
+  *
+  * @param self Module that is calling this function
+  * @param args It is then parsed to a single dictionary {atom_id:(x, y, z)}
+
+  * @returns Python dictionary {atom_id:(tuple of descriptors)}
+  */
 static PyObject *descriptors_compute(PyObject* self, PyObject *args) {
 
     // parse args to atoms
@@ -21,7 +26,7 @@ static PyObject *descriptors_compute(PyObject* self, PyObject *args) {
     Py_ssize_t  pos1            = 0;
 
     std::vector<int>                    vector_atom_id;
-    std::vector<std::vector<floadescriptorsmodule.cppt>>     vector_atom_coords;
+    std::vector<std::vector<float>>     vector_atom_coords;
 
     // iterate through {atoms_id:coords} and get C vectors
     while (PyDict_Next(atoms, &pos1, &atom_ids, &coordinates)) {
@@ -45,15 +50,21 @@ static PyObject *descriptors_compute(PyObject* self, PyObject *args) {
     //  1. atom ids (1, 2, 3, 6, 90, 900, 96, ...)
     //  2. corresponding tuples of coordinates ((3, 4, 5), (3, 5, 7), (6, 7, 0), ...)
 
-    // I need to calculate descriptors!
-    // ...
-
+    // TODO: I need to calculate descriptors!
 
     // return a dictionary {atom_id:descriptors}
     return Py_BuildValue("d", 0);
 }
 
-// convert PyObject to C++ Vector
+/**
+ * Converts PyObject(List/Tuple) to C++ Vector
+ *
+ * @param incoming It is a PyObject (PyTuple, PyList)
+ *
+ * @returns data Vector
+ *
+ * @throws Error when incoming was neither PyTuple, nor PyList
+ */
 std::vector<float> listTupleToVector_Float(PyObject* incoming) {
 	std::vector<float> data;
 	if (PyTuple_Check(incoming)) {
@@ -74,25 +85,32 @@ std::vector<float> listTupleToVector_Float(PyObject* incoming) {
 	return data;
 }
 
-/*
- *  Other stuff needed to compile module
- */
+// Other stuff needed to compile module
 
+/**
+ * Defines methods that can be called from Python
+ */
 static PyMethodDef descriptors_methods[] = {
     // "PythonName",    C-function name,        argument presentation,  description
     {"compute",         descriptors_compute,    METH_VARARGS,           "Compute descriptors"},
     {NULL, NULL, 0, NULL}   /* SENTINEL */
 };
 
+/**
+ * Structure that holds information about module
+ */
 static struct PyModuleDef descriptors =
 {
     PyModuleDef_HEAD_INIT,
-    "decsriptors",      // name of module
+    "descriptors",      // name of module
     NULL,               // module documentation
     -1,                 // size of per-interpreter state of the module, or -1 if the module keeps state in global variables.
     descriptors_methods
 };
 
+/**
+ * Initialization class of C++ extension for Python
+ */
 PyMODINIT_FUNC PyInit_descriptors(void)
 {
     return PyModule_Create(&descriptors);
--- src/modules/descriptors/descriptorsmodule.h
+++ src/modules/descriptors/descriptorsmodule.h
@@ -2,4 +2,5 @@ extern "C" {
     #include <Python.h>
 }
 
+static PyObject *descriptors_compute(PyObject* self, PyObject *args);
 std::vector<float> listTupleToVector_Float(PyObject* incoming);
