4f9bdad2313cddde1e681e1e5da2d1d25f8e1ada
ondrej17 <bily.ondrej1701@gmail.com>
2020-11-17 18:57:52 +0100

    Changes in CMakeLists.txt made. Added tests

---
+++ src/modules/descriptors/tests/test_atom.cpp
@@ -0,0 +1,15 @@
+/**
+ * Tests for Atom class
+ * 
+ * 
+ */
+
+#include <iostream>
+
+#include "../src/descriptors_atom.h"
+
+int main() {
+    std::cout << "Testing Atom class ...\n";
+    return 0;
+}
+
---
+++ src/modules/descriptors/tests/test_box.cpp
@@ -0,0 +1,15 @@
+/**
+ * Tests for Box class
+ * 
+ * 
+ */
+
+#include <iostream>
+
+#include "../src/descriptors_box.h"
+
+int main() {
+    std::cout << "Testing Box class ...\n";
+    return 0;
+}
+
